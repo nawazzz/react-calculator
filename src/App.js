eca51dd5fc8584eb0275f6ab4730299c4f11cde0
nawazzz <65383832+nawazzz@users.noreply.github.com>
2021-09-12 17:04:19 +0530

    add calculator functionality

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.scss';
 import React from 'react';
 
@@ -13,76 +12,87 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      squares: ["AC", "+", "-", "x", "/", "%", 9, 8, 7, 6, 5, 4, 3, 2, 1, 0, "=", "del"],
-      clickedValueOne: '',
-      clickedValueTwo: '',
-      displayValue: '',
-      operatorValue: '',
+      ops: ['/', '*', '-', '+', '.'],
+      calc: "",
+      result: "",
     }
   }
 
-  handleDigits = (e) => {
-    console.log(e)
-    if ((this.state.operatorValue.length === 0) && !["x" , "+", "-", "/", "%", "del", "AC"].includes(e.target.dataset.name)) {
-      console.log('first block')
+  createDigits = () => {
+		const digits = [];
+		for (let i = 1; i < 10; i++) {
+			digits.push(<button 
+          onClick={(e) => this.setState((prevState) => {
+            return {
+              calc: prevState.calc + i.toString() 
+            }
+          })} key={i}>
+            {i}
+        </button>);
+		}
+
+		return digits;
+	}
+
+  updateCalc = (value) => {
+    if(
+      (this.state.ops.includes(value) && this.state.calc === "") || 
+      (this.state.ops.includes(value) && this.state.ops.includes(this.state.calc.slice(-1)))
+    ) {
+      return
+    }
+    this.setState((prevState) => {
+      return {
+        calc: prevState.calc + value
+      }
+    })
+    if (!this.state.ops.includes(value)) {
       this.setState((prevState) => {
         return {
-        clickedValueOne: prevState.clickedValueOne + e.target.innerText
+          result: eval(prevState.calc + value).toString()
         }
-      }, () => {console.log(this.state)})
-    }
-
-    if (["x" , "+", "-", "/", "%"].includes(e.target.dataset.name) && e.target.dataset.name !== "=") {
-      console.log('second block')
-
-      this.setState({
-        operatorValue: e.target.dataset.name
       })
     }
-
-    if ((this.state.operatorValue.length !== 0) && ["x" , "+", "-", "/", "%", "del", "AC", "="].includes(e.target.dataset.name) === false) {
-      console.log('third block')
-
-      this.setState((prevState) => {
-        return {
-          clickedValueTwo: prevState.clickedValueTwo + e.target.innerText
-        }
-      }, () => {console.log(this.state.clickedValueTwo)})
-    }
-
-    if ((this.state.operatorValue) && (this.state.clickedValueOne) && (this.state.clickedValueTwo) && ["x" , "+", "-", "/", "%", "del", "AC", "="].includes(e.target.dataset.name)) {
-      console.log('4th block')
-      console.log(eval(this.state.clickedValueOne + this.state.operatorValue + this.state.clickedValueTwo))
-
-          this.setState({
-            clickedValueOne: eval((this.state.clickedValueOne + this.state.operatorValue + this.state.clickedValueTwo))
-          })
-    }
   }
 
+  calculate = () => {
+    this.setState((prevState) => {
+      return {
+        calc: eval((this.state.calc).toString())
+      }
+    })
+  }
 
+  deleteLast = () => {
+    if (this.state.calc === "") {
+      return
+    } 
+    const value = this.state.calc.toString().slice(0, -1);
+    this.setState({
+      calc: value
+    })
+  }
 
   render() {
     return (
-      <React.Fragment>
-        <div>
-          <input type="text" value={this.state.clickedValueOne}style={{width: "300px", height: "50px"}}>
-
-          </input>
+      <div style={{width: "180px", margin: "0 auto"}}>
+        <div className="display">
+					<span>{this.state.result ? '(' + this.state.result + ')' : ''}</span> {this.state.calc || 0}
+				</div>
+        <div className="operators">
+          <button className="operatorButtons" onClick={() => this.updateCalc("/")}>/</button>
+          <button className="operatorButtons" onClick={() => this.updateCalc("*")}>x</button>
+          <button className="operatorButtons" onClick={() => this.updateCalc("+")}>+</button>
+          <button className="operatorButtons" onClick={() => this.updateCalc("-")}>-</button>
+          <button className="operatorButtons" onClick={this.deleteLast}>DEL</button>
         </div>
-        <div className="calculatorUI">
-          {this.state.squares.map((elm, index) => {
-            return(
-                <div className="digitSquare"
-                     onClick={this.handleDigits}
-                     data-name={elm}
-                >
-                  {elm}
-                </div>
-            )
-          })}
+        <div className="digits">
+          {this.createDigits()}
+          <button style={{borderBottomLeftRadius: "8px"}} className="operatorButtons" onClick={() => this.setState({calc: "0"})}>0</button>
+          <button className="operatorButtons" onClick={() => this.updateCalc(".")}>.</button>
+          <button style={{borderBottomRightRadius: "8px"}} className="operatorButtons" onClick={this.calculate}>=</button>
         </div>
-      </React.Fragment>
+      </div>
     );
   }
 }
